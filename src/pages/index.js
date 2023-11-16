a16e738662027183b3cf485883070485d9a61d78
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-11-16 23:47:45 +0700

    My Website

---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className='container'>
+        <Heading as='h1' className='hero__title'>
+          {siteConfig.title}
+        </Heading>
+        <p className='hero__subtitle'>
+          This is my website for learning and documenting my journey as a
+          software engineer.
+        </p>
+        <div>
+          <Link
+            className='button button--secondary button--lg'
+            to='/docs/intro'
+          >
+            See All My Documentation
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  return (
+    <Layout title='My Website'>
+      <HomepageHeader />
+    </Layout>
+  );
+}
